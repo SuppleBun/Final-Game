6d82511369735d4ba3a5317ca3a02131815e3000
Mingun Cho <mingun0108@gmail.com>
2020-05-30 19:29:51 -0700

    Added track walls, object walls

--- src/scenes/PlayS.js
+++ src/scenes/PlayS.js
@@ -74,20 +74,325 @@ class PlayS extends Phaser.Scene {
         //this.garage = this.add.image(2100, 3500, 'garage').setScale(2);
         //this.hallway = this.add.image(700, 2100, 'hallway').setScale(2);
 
+        // OUTER WALLS -----------------------------------------------------
+        this.wall = this.matter.add.sprite(110, -1245);
+        this.wall.setBody({
+            type: 'rectangle',
+            width: 1580,
+            height: 10
+        })
+        this.wall.setStatic(true);
+
+        this.wall2 = this.matter.add.sprite(-780, -1150);
+        this.wall2.setBody({
+            type: 'rectangle',
+            width: 260,
+            height: 10,
+        })
+        this.wall2.rotation = 2.35;
+        this.wall2.setStatic(true);
+
+        this.wall3 = this.matter.add.sprite(-860, -40);
+        this.wall3.setBody({
+            type: 'rectangle',
+            width: 2035,
+            height: 10,
+        })
+        this.wall3.rotation = 1.5708; // 90 degrees
+        this.wall3.setStatic(true);
+
+        this.wall4 = this.matter.add.sprite(-760, 1100);
+        this.wall4.setBody({
+            type: 'rectangle',
+            width: 330,
+            height: 10,
+        })
+        this.wall4.rotation = -2.25;
+        this.wall4.setStatic(true);
+
+        this.wall5 = this.matter.add.sprite(-60, 1230);
+        this.wall5.setBody({
+            type: 'rectangle',
+            width: 1220,
+            height: 10
+        })
+        this.wall5.setStatic(true); 
+
+        // Made a mistake of leaving a wall in the middle
+        this.wall_middle = this.matter.add.sprite(710, 1070);
+        this.wall_middle.setBody({
+            type: 'rectangle',
+            width: 470,
+            height: 10,
+        })
+        this.wall_middle.rotation = 2.36;
+        this.wall_middle.setStatic(true);
+
+        this.wall6 = this.matter.add.sprite(875, 590);
+        this.wall6.setBody({
+            type: 'rectangle',
+            width: 650,
+            height: 10,
+        })
+        this.wall6.rotation = 1.5708; // 90 degrees
+        this.wall6.setStatic(true);
+
+        this.wall7 = this.matter.add.sprite(900, 245);
+        this.wall7.setBody({
+            type: 'rectangle',
+            width: 70,
+            height: 10,
+        })
+        this.wall7.rotation = 2.35; // 45 degrees
+        this.wall7.setStatic(true);
+
+        this.wall8 = this.matter.add.sprite(1030, 223);
+        this.wall8.setBody({
+            type: 'rectangle',
+            width: 220,
+            height: 10,
+        })
+        this.wall8.setStatic(true);
+
+        this.wall9 = this.matter.add.sprite(1175, 180);
+        this.wall9.setBody({
+            type: 'rectangle',
+            width: 120,
+            height: 10,
+        })
+        this.wall9.rotation = 2.35; // 45 degrees
+        this.wall9.setStatic(true);
+
+        this.wall10 = this.matter.add.sprite(1220, -110);
+        this.wall10.setBody({
+            type: 'rectangle',
+            width: 520,
+            height: 10,
+        })
+        this.wall10.rotation = 1.575; 
+        this.wall10.setStatic(true);
+
+        this.wall11 = this.matter.add.sprite(1115, -475);
+        this.wall11.setBody({
+            type: 'rectangle',
+            width: 293,
+            height: 10,
+        })
+        this.wall11.rotation = 0.8; // 135 degrees
+        this.wall11.setStatic(true);
+
+        this.wall12 = this.matter.add.sprite(1015, -860);
+        this.wall12.setBody({
+            type: 'rectangle',
+            width: 580,
+            height: 10,
+        })
+        this.wall12.rotation = 1.5708; // 90 degrees
+        this.wall12.setStatic(true);
+
+        this.wall13 = this.matter.add.sprite(960, -1190);
+        this.wall13.setBody({
+            type: 'rectangle',
+            width: 165,
+            height: 10,
+        })
+        this.wall13.rotation = 0.8; // 135 degrees
+        this.wall13.setStatic(true);
+        // OUTER WALLS -----------------------------------------------------
+
+        // INNER WALLS -----------------------------------------------------
+        this.wall14 = this.matter.add.sprite(170, -1035);
+        this.wall14.setBody({
+            type: 'rectangle',
+            width: 960,
+            height: 10
+        })
+        this.wall14.setStatic(true);
+
+        this.wall15 = this.matter.add.sprite(-360, -990);
+        this.wall15.setBody({
+            type: 'rectangle',
+            width: 137,
+            height: 10,
+        })
+        this.wall15.rotation = 2.35;
+        this.wall15.setStatic(true);
+
+        this.wall16 = this.matter.add.sprite(-400, -20);
+        this.wall16.setBody({
+            type: 'rectangle',
+            width: 1850,
+            height: 10,
+        })
+        this.wall16.rotation = 1.5708; // 90 degrees
+        this.wall16.setStatic(true);
+
+        this.wall17 = this.matter.add.sprite(-370, 945);
+        this.wall17.setBody({
+            type: 'rectangle',
+            width: 100,
+            height: 10,
+        })
+        this.wall17.rotation = -2.25;
+        this.wall17.setStatic(true);
+
+        this.wall18 = this.matter.add.sprite(25, 985);
+        this.wall18.setBody({
+            type: 'rectangle',
+            width: 728,
+            height: 10
+        })
+        this.wall18.setStatic(true); 
+
+        // inside of the mistake wall
+        this.wall_middleinside = this.matter.add.sprite(485, 885);
+        this.wall_middleinside.setBody({
+            type: 'rectangle',
+            width: 280,
+            height: 10,
+        })
+        this.wall_middleinside.rotation = 2.35;
+        this.wall_middleinside.setStatic(true);
+
+        this.wall19 = this.matter.add.sprite(585, 400);
+        this.wall19.setBody({
+            type: 'rectangle',
+            width: 777,
+            height: 10,
+        })
+        this.wall19.rotation = 1.5708; // 90 degrees
+        this.wall19.setStatic(true);
+
+        this.wall20 = this.matter.add.sprite(625, -30);
+        this.wall20.setBody({
+            type: 'rectangle',
+            width: 120,
+            height: 10,
+        })
+        this.wall20.rotation = 2.35; // 45 degrees
+        this.wall20.setStatic(true);
+
+        this.wall21 = this.matter.add.sprite(785, -70);
+        this.wall21.setBody({
+            type: 'rectangle',
+            width: 235,
+            height: 10,
+        })
+        this.wall21.setStatic(true);
+
+        this.wall22 = this.matter.add.sprite(915, -80);
+        this.wall22.setBody({
+            type: 'rectangle',
+            width: 35,
+            height: 10,
+        })
+        this.wall22.rotation = 2.55; 
+        this.wall22.setStatic(true);
+
+        this.wall23 = this.matter.add.sprite(925, -200);
+        this.wall23.setBody({
+            type: 'rectangle',
+            width: 230,
+            height: 10,
+        })
+        this.wall23.rotation = 1.575; 
+        this.wall23.setStatic(true);
+
+        this.wall24 = this.matter.add.sprite(840, -400);
+        this.wall24.setBody({
+            type: 'rectangle',
+            width: 260,
+            height: 10,
+        })
+        this.wall24.rotation = 0.78; // 135 degrees
+        this.wall24.setStatic(true);
+
+        this.wall25 = this.matter.add.sprite(750, -710);
+        this.wall25.setBody({
+            type: 'rectangle',
+            width: 450,
+            height: 10,
+        })
+        this.wall25.rotation = 1.5708; // 90 degrees
+        this.wall25.setStatic(true);
+
+        this.wall26 = this.matter.add.sprite(700, -980);
+        this.wall26.setBody({
+            type: 'rectangle',
+            width: 145,
+            height: 10,
+        })
+        this.wall26.rotation = 0.79; // 135 degrees
+        this.wall26.setStatic(true);
+        // INNER WALLS -----------------------------------------------------
+
+        // OBJECTS ON TRACK ------------------------------------------------
+        this.door = this.matter.add.sprite(1170, -205);
+        this.door.setBody({
+            type: 'rectangle',
+            width: 245,
+            height: 20,
+        })
+        this.door.rotation = 1.295;
+        this.door.setStatic(true);
+
+        this.door2 = this.matter.add.sprite(-110, -1220);
+        this.door2.setBody({
+            type: 'rectangle',
+            width: 173,
+            height: 13,
+        })
+        this.door2.rotation = 2.97;
+        this.door2.setStatic(true);
+
+        this.toy = this.matter.add.sprite(799, 736);
+        this.toy.setBody({
+            type: 'rectangle',
+            width: 47,
+            height: 35,
+        })
+        this.toy.setStatic(true);
+
+        this.toy2 = this.matter.add.sprite(805, 575);
+        this.toy2.setBody({
+            type: 'rectangle',
+            width: 47,
+            height: 34,
+        })
+        this.toy2.setStatic(true);
+
+        this.toy3 = this.matter.add.sprite(749, 136);
+        this.toy3.setBody({
+            type: 'rectangle',
+            width: 47,
+            height: 35,
+        })
+        this.toy3.setStatic(true);
+
+        this.toy4 = this.matter.add.sprite(598, 1040);
+        this.toy4.setBody({
+            type: 'rectangle',
+            width: 47,
+            height: 35,
+        })
+        this.toy4.setStatic(true);
+        // OBJECTS ON TRACK ------------------------------------------------
+
         // Add item box (for now)
-        this.item_box = this.matter.add.sprite(-150, -150, 'item_box').setScale(1.5);
+        this.item_box = this.matter.add.sprite(650, 200, 'item_box');
+        //this.item_box.setStatic(true);
 
         // Hitbox for item box
         this.item_box.setBody({
             type: 'rectangle',
-            width: 64,
-            height: 64
+            width: 48,
+            height: 48
         })
 
         this.item_box.anims.play("box_rotate");
 
         // Add player
-        this.player = this.matter.add.sprite(0, 0, 'player1').setOrigin(0.5, 0).setScale(2);
+        this.player = this.matter.add.sprite(650, 400, 'player1').setOrigin(0.5, 0).setScale(2);
 
         // Hitbox for player
         this.player.setBody({
@@ -95,6 +400,8 @@ class PlayS extends Phaser.Scene {
             width: 50,
             height: 94
         })
+
+        //this.player.setCollisionGroup(-1);
 
         // Steeringwheel for player
         this.SteeringWheel = this.matter.add.sprite(-2425, -2095, 'UI_steeringwheel').setScale(2.5);
@@ -105,7 +412,7 @@ class PlayS extends Phaser.Scene {
         this.SteeringWheelAnim2 = true;
 
         // Add player2
-        this.player2 = this.matter.add.sprite(150, 0, 'player2').setOrigin(0.5, 0).setScale(2);
+        this.player2 = this.matter.add.sprite(800, 400, 'player2').setOrigin(0.5, 0).setScale(2);
 
         // Hitbox for player2
         this.player2.setBody({
@@ -123,7 +430,7 @@ class PlayS extends Phaser.Scene {
         this.SteeringWheelAnim4 = true;
 
         // Prevent players moving during countdown
-        this.canMove = false;
+        this.canMove = true; // should be false
 
         // Prevent acceleration sound playing again
         this.acceleration_play = true;
@@ -162,7 +469,7 @@ class PlayS extends Phaser.Scene {
         this.camera = this.cameras.main.setViewport(0, 0, (game.config.width/2), game.config.height);
         this.camera.startFollow(this.player, true, 1, 1, 0, 0);
         this.camera.setZoom(0.6);
-        //this.camera.setZoom(1);
+        //this.camera.setZoom(0.3);
 
         //this.camera.ignore(this.player_UI);
         this.camera2 = this.cameras.add(centerX, 0, (game.config.width/2), game.config.height);
@@ -191,12 +498,12 @@ class PlayS extends Phaser.Scene {
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
-        this.engineIdle = this.sound.add('engineIdle_sfx', {volume: 0.25, loop: true});
+        /*this.engineIdle = this.sound.add('engineIdle_sfx', {volume: 0.25, loop: true});
         this.engineIdle.play();
 
         // Play the engine On sound
         this.engineStart = this.sound.add('engineStart_sfx', {volume: 0.25});
-        this.engineStart.play();
+        this.engineStart.play();*/
 
         // Play the countdown animation
         let countdown = this.add.sprite(this.player.x + 32, this.player.y - 100, 'start_light');
@@ -206,10 +513,10 @@ class PlayS extends Phaser.Scene {
             // Players can now move
             this.canMove = true;
 
-            // Play bgm
+            /*// Play bgm
             this.bgm = this.sound.add('bgm', { volume: 0.3 });
             this.bgm.setLoop(true);
-            this.bgm.play();
+            this.bgm.play();*/
 
             // Play 'Go!' and firework effect
             let go_effect = this.add.sprite(this.player.x, this.player.y, 'go_effect');
@@ -227,7 +534,7 @@ class PlayS extends Phaser.Scene {
 
         })
 
-        // delay before game starts
+        /*// delay before game starts
         this.time.addEvent({
             delay: 1000, // 1 second 
             callback: () => {
@@ -236,7 +543,7 @@ class PlayS extends Phaser.Scene {
                 this.raceStart.play();
             },
             loop: false
-        })
+        })*/
 
     }
 
@@ -286,14 +593,14 @@ class PlayS extends Phaser.Scene {
             if (keyUP.isDown) {
                 this.carSpeed += 0.26;
 
-                // Play the acceleration sound
+                /*// Play the acceleration sound
                 if(this.acceleration_play){
                     this.acceleration.play();
                     this.acceleration_play = false;
                     this.acceleration.on('complete', () => {
                         this.acceleration_play = true;
                     })
-                }
+                }*/
             }
             else {
                 if (this.carSpeed >= 0) {
