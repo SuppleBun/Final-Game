fb00cebd254acb8b4ff735fa0ca5fc928e4c12d6
Joonsu Jun <jojun@ucsc.edu>
2020-06-06 15:19:46 -0700

    added credits scene and updated menu scene

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -8,7 +8,45 @@ class Credits extends Phaser.Scene {
     }
    
     create() {
-        this.add.text(20, 120, "Credits");
+        // Credits title display
+        let titleConfig = {
+            fontFamily: 'Impact',
+            fontSize: '65px',
+            color: '#ffffff',
+            align: 'right',
+            stroke: '#000000',
+            strokeThickness: 6,
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWith: 0
+        }
+
+        // Description display
+        let optionsConfig = {
+            fontFamily: 'Impact',
+            fontSize: '30px',
+            color: '#ffffff',
+            align: 'right',
+            stroke: '#000000',
+            strokeThickness: 6,
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWith: 0
+        }
+
+        // display menu text
+        let centerX = game.config.width / 2;
+        let centerY = game.config.height / 2;
+        let textSpacer = 100;
+
+        this.add.text(centerX, centerY - textSpacer, 'Credits', titleConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + 10, 'Created by Tekkaden', optionsConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + 55, 'Mingun Cho, Joonsu Jun, Sean Kang', optionsConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + 100, 'UCSC Spring 2020 CMPM/ARTG 120', optionsConfig).setOrigin(0.5);
 
         // define keys
         keyONE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -58,6 +58,7 @@ class Menu extends Phaser.Scene {
             fixedWith: 0
         }
 
+        // options display
         let optionsConfig = {
             fontFamily: 'Impact',
             fontSize: '40px',
@@ -72,6 +73,21 @@ class Menu extends Phaser.Scene {
             fixedWith: 0
         }
 
+
+        // asterisk display
+        let asteriskConfig = {
+            fontFamily: 'Impact',
+            fontSize: '20px',
+            color: '#ffffff',
+            align: 'right',
+            stroke: '#000000',
+            strokeThickness: 6,
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWith: 0
+        }
         // display menu text
         let centerX = game.config.width / 2;
         let centerY = game.config.height / 2;
@@ -81,6 +97,7 @@ class Menu extends Phaser.Scene {
         this.add.text(centerX, centerY + 10, 'Press 1 to play', optionsConfig).setOrigin(0.5);
         this.add.text(centerX, centerY + 55, 'Press 2 for tutorial', optionsConfig).setOrigin(0.5);
         this.add.text(centerX, centerY + 100, 'Press 3 for credits', optionsConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + 200, '*Click on the screen if background music fails to play', asteriskConfig).setOrigin(0.5);
 
         // define keys
         keyONE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
